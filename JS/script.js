eef4558bf2cf55a82ba89861a3068365ed61ecb7
Josan Johnata <josanjohnata@gmail.com>
2021-09-14 10:51:12 -0300

    Tipos de dados JS String

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+/*
+  String
+
+  * Cadeia de caracteres
+
+  "" // aspas duplas
+  console.log("Josan")
+
+  '' // aspas simples
+  console.log('Johnata')
+  ou
+  console.log('"Josan""Johnata"')
+
+
+  `` // template literals ou template strings
+  Permite colocar expressões de código(concatenar) e escrever em multiplas linhas
+  console.log(`Seja bem vindo ${name}`)
+  console.log(`
+    Seja bem vindo ${name}
+    Será uma prazer ter você no ${event}
+    `)
+
+*/
+
