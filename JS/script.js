47c00aa81d104f6dff7eceb8c3417ff06ba104fb
Josan Johnata <josanjohnata@gmail.com>
2021-09-19 15:19:40 -0300

    Revisao de Objetos

--- JS/script.js
+++ JS/script.js
@@ -75,6 +75,14 @@
     
   { propriedade: "valor" }
 
+  const person {
+    name: "John",
+    age: 29,
+    weight: 88.6,
+    isAdmin: true
+  };
+  console.log(person.name);
+
     console.log({
       name : "Josan",
       age: 29,
