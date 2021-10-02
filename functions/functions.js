1a79b55e10bb48cf358e24193cbd338f056c2ab4
Josan Johnata <josanjohnata@gmail.com>
2021-10-02 13:04:45 -0300

    Retornando valores dentro da função

--- functions/functions.js
+++ functions/functions.js
@@ -26,3 +26,18 @@ const sum = function(number1, number2) {
 }
 
 sum(2, 3); // argumentos -  arguments
+
+// Retornando valores dentro da função
+
+const sum = function(number1, number2) {
+  let total = number1 + number2;
+  return total;
+}
+
+let valor1 = 36;
+let valor2 = 64;
+
+console.log(`O valor 1 é ${valor1}`);
+console.log(`O valor 2 é ${valor2}`);
+console.log(`A soma dos valores é ${sum(valor1, valor2)}`);
+
