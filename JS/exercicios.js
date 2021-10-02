30ca00f7600d89a4005fa9305abbc9a64b54b6ff
Josan Johnata <josanjohnata@gmail.com>
2021-10-02 11:42:45 -0300

    Started rocketseat exercises - exercise 3

--- JS/exercicios.js
+++ JS/exercicios.js
@@ -5,3 +5,24 @@ let weight;
 // 2- Qeu tipo de dado é a variável acima?
 
 console.log(typeof weight);
+
+/*
+  3- Declare uma variável e atribua valores para cada um dos dados:
+    * name: String
+    * age: Number (integer)
+    * stars: Number (float)
+    * inSubscribed: Boolean
+*/
+
+channelYoutube = {
+  name: 'O mundo do John',
+  age: 5,
+  stars: 4.9,
+  inSubscribed: true
+}
+// or
+
+let name = 'John';
+let age = 29;
+let stars = 4.9;
+let inSubscribed = true;
