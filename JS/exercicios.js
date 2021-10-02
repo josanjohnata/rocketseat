9fb8770349939912ab90ca38aa8149d500f861a6
Josan Johnata <josanjohnata@gmail.com>
2021-10-02 11:42:54 -0300

    Started rocketseat exercises - exercise 4

--- JS/exercicios.js
+++ JS/exercicios.js
@@ -26,3 +26,20 @@ let name = 'John';
 let age = 29;
 let stars = 4.9;
 let inSubscribed = true;
+
+/*
+  4- A variável student abaixo é de que tipo de dados.
+    4.1 Atribua a ela as mesmas propriedades e valores do exercícios 3.
+    4.2 Mostre no console a seguinte mensagem:
+        <name> de idade <age> pesa <weight> kg.
+        Atenção! Substitua <name> <age> e <weight> pelos valores de cada propriedade do objeto.
+*/
+
+student = {
+  name: 'Josan',
+  age: 29,
+  course: 'Web Development',
+  weight: 90
+}
+
+console.log(`O estudante ${student.name} tem ${student.age} anos, pesa ${student.weight}kg e estuda ${student.course}.`);
