857c72854c3e2827cebba6525b92621718bb1bbe
josanjohnata <josan.potes@meliuz.com.br>
2022-07-28 22:10:51 -0300

    Adiciona componente Sidebar

---
+++ reactJs/fundamentos-react-js/src/components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+            />
+
+            <div className={styles.profile}>
+                <img
+                    className={styles.avatar}
+                    src='https://avatars.githubusercontent.com/u/64118585?v=4'
+                />
+
+                <strong>Josan Johnata</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer className={styles.footer}>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
