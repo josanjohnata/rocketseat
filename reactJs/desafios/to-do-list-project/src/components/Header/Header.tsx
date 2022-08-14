6e18d478d253029bf6faedba288caf44b8840b10
josanjohnata <josan.potes@meliuz.com.br>
2022-08-14 20:00:22 -0300

    Adiciona componete Header

---
+++ reactJs/desafios/to-do-list-project/src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import todoLogo from '../../assets/rocket.png';
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={todoLogo} alt="Rocket"/>
+      <h1>
+        <span className={styles.to}>to</span>
+        <span className={styles.do}>do</span>
+      </h1>
+    </header>
+  )
+}
+
+export default Header;
