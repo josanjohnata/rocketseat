a69ea2455ccc4246c9235053eec4b466d4732dce
josanjohnata <josan.potes@meliuz.com.br>
2022-08-20 18:49:18 -0300

    Adiciona a funcionalidade de countDone e countTodo

--- reactJs/desafios/to-do-list-project/src/components/Content/Content.tsx
+++ reactJs/desafios/to-do-list-project/src/components/Content/Content.tsx
@@ -1,42 +1,61 @@
 import styles from "./Content.module.css";
 
-import { Trash } from "phosphor-react";
+import { CheckCircle, Circle, Trash } from "phosphor-react";
 import { useState } from "react";
 
 interface TasksProps {
   content: string;
   onDeleteTask: (comment: string) => void;
+  onHandleTaskDone: (comment: number) => void;
+  onHandleTaskToDO: (comment: number) => void;
 }
 
-function Content({content, onDeleteTask}: TasksProps) {
+function Content({ content, onDeleteTask, onHandleTaskDone, onHandleTaskToDO }: TasksProps) {
   const [isChecked, setIsChecked] = useState(false);
 
-  const handleDeleteTask = () => {
+  function handleDeleteTask() {
     onDeleteTask(content);
   };
 
+  function handleTaskDone() {
+    onHandleTaskDone(content);
+    setIsChecked(!isChecked);
+  };
+
+  function handleTaskToDO() {
+    onHandleTaskToDO(content);
+    setIsChecked(!isChecked);
+  }
+
   return (
     <div className={styles.newTaskInfo}>
-      <div>
-        <div className={styles.newTask}>
-          <div className={styles.newTaskItem}>
-            <input
-              type="checkbox"
-              id="myTask"
-              onChange={() => {
-                setIsChecked(!isChecked);
-              }}
-            />
-            <label
-              htmlFor="myTask"
-            >
-              <p className={isChecked === true ? styles.checkbox : styles.checkboxUnchecked}>{content}</p>
-            </label>
-          </div>
-          <button onClick={handleDeleteTask}>
-            <Trash />
-          </button>
+      <div className={styles.newTask}>
+        <div className={styles.newTaskItem}>
+          <a
+            onClick={isChecked ? handleTaskDone : handleTaskToDO}
+
+          >
+            {isChecked === true ? (
+              <CheckCircle size={20} color="#5E60CE" weight="bold" />
+            ) : (
+              <Circle
+                size={20}
+                color="#4EA8DE"
+                weight="bold"
+              />
+            )}
+          </a>
+          <p
+            className={
+              isChecked === true ? styles.checkbox : styles.checkboxUnchecked
+            }
+          >
+            {content}
+          </p>
         </div>
+        <button onClick={handleDeleteTask}>
+          <Trash />
+        </button>
       </div>
     </div>
   );
