ebde2efc55237e84644bc400812970bb88741de0
josanjohnata <josan.potes@meliuz.com.br>
2022-08-29 12:19:52 -0300

    Introdução ao style componets

---
+++ react-modulo-2/02-ignite-time/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button";
+
+export function App() {
+  return (
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+    </>
+  );
+}
