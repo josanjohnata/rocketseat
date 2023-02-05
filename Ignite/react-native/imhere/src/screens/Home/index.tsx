75ebf8a5a7603199d3482f7c4f9b958d1f170d9d
Josan Johnata <josan.pontes@meliuz.com.br>
2023-02-05 16:27:34 -0300

    Finalizando modulo 1 - Im Here - React native

--- Ignite/react-native/imhere/src/screens/Home/index.tsx
+++ Ignite/react-native/imhere/src/screens/Home/index.tsx
@@ -2,21 +2,27 @@ import { View, Text, TextInput, TouchableOpacity, FlatList, Alert } from "react-
 
 import { styles } from "./styles";
 import { Participant } from "../../components/Participant";
+import { useState } from "react";
 
 export function Home() {
-  const participants = ['Josan', 'Eide', 'Ester', 'Milk', 'Nicolly', 'Tauanne', 'Maria', 'João', 'Victor']
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState('');
 
   const handleParticipantAdd = () => {
-    if(participants.includes('Josan')) {
+    if(participants.includes(participantName)) {
       return Alert.alert('Participante existe', 'Já existe um participante na lista com esse nome!')
     }
+
+    setParticipants(prevState => [...prevState, participantName]);
+    setParticipantName('');
   };
 
   const handleParticipantRemove = (name: string) => {
-    Alert.alert('Remover', `Remover o participante ${name}?`, [
+     Alert.alert('Remover', `Remover o participante ${name}?`, [
       {
         text: 'Sim',
-        onPress: () => Alert.alert('Deletado!')
+        onPress: () => setParticipants(prevState => prevState
+          .filter(participant => participant !== name))
       },
       {
         text: 'Não',
@@ -40,6 +46,8 @@ export function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#6B6B6B"
+          onChangeText={setParticipantName}
+          value={participantName}
         />
 
         <TouchableOpacity
