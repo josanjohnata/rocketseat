e28be91cc31abe5285ec9cc42a85d61f04722ffe
Josan Johnata <josan.pontes@meliuz.com.br>
2023-02-11 15:16:29 -0300

    feat: Change background body

--- Ignite/react-native/desafios/todolist/App.tsx
+++ Ignite/react-native/desafios/todolist/App.tsx
@@ -19,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1A1A1A',
     alignItems: 'center',
     justifyContent: 'center',
   },
