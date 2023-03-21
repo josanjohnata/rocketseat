cb326b25b92da6262c72e35f519cad77d25a12ae
Josan Johnata <josan.pontes@meliuz.com.br>
2023-03-21 09:50:00 -0300

    feat: Adiciona a tela Groups

--- Ignite/react-native/modulos/igniteteams/App.tsx
+++ Ignite/react-native/modulos/igniteteams/App.tsx
@@ -1,6 +1,8 @@
-import { ActivityIndicator } from 'react-native';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
 
 import theme from './src/theme';
 
@@ -11,7 +13,12 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
