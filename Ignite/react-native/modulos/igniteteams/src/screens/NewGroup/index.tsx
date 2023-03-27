ca9eb1d5554b8c278400b599945076d34a8006ff
Josan Johnata <josan.pontes@meliuz.com.br>
2023-03-27 11:00:37 -0300

    feat: Add Input Component

--- Ignite/react-native/modulos/igniteteams/src/screens/NewGroup/index.tsx
+++ Ignite/react-native/modulos/igniteteams/src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@components/Header";
 import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -16,8 +17,11 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
+        <Input />
+
         <Button
           title="Criar"
+          style={{ marginTop: 25 }}
         />
       </Content>
     </Container>
