11aa385c5d9b25300545b407e1a09225a169a094
Josan Johnata <josan.pontes@meliuz.com.br>
2023-03-28 09:04:09 -0300

    refactor: Reafatora a importação do tema

--- Ignite/react-native/modulos/igniteteams/src/components/GroupCard/styles.ts
+++ Ignite/react-native/modulos/igniteteams/src/components/GroupCard/styles.ts
@@ -1,5 +1,5 @@
 import { TouchableOpacity } from "react-native";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { UsersThree } from "phosphor-react-native";
 
 export const Container = styled(TouchableOpacity)`
@@ -19,9 +19,12 @@ export const Container = styled(TouchableOpacity)`
 export const Title = styled.Text`
   text-align: center;
 
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-  color: ${({ theme }) => theme.COLORS.GRAY_200};
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    color: ${theme.COLORS.GRAY_200};
+  `};
+
 `;
  export const Icon = styled(UsersThree).attrs(({ theme }) => ({
   size: 32,
--- Ignite/react-native/modulos/igniteteams/src/screens/NewGroup/index.tsx
+++ Ignite/react-native/modulos/igniteteams/src/screens/NewGroup/index.tsx
@@ -17,7 +17,7 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Input />
+        <Input placeholder='Nome da turma'/>
 
         <Button
           title="Criar"
