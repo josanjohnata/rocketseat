295de5ac7111fcf73e8a8761dc15f9baa7db12a3
Josan Johnata <josan.pontes@meliuz.com.br>
2023-03-30 19:31:38 -0300

    feat: Add Players Components

--- Ignite/react-native/modulos/igniteteams/App.tsx
+++ Ignite/react-native/modulos/igniteteams/App.tsx
@@ -6,7 +6,7 @@ import { Loading } from '@components/Loading';
 
 import theme from './src/theme';
 
-import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -18,7 +18,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <NewGroup /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
