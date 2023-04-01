a52b10d2f67dbced92ca0bacb19e1312d88eb043
Josan Johnata <josan.pontes@meliuz.com.br>
2023-04-01 12:10:24 -0300

    feat: Add Button Icon Components

--- Ignite/react-native/modulos/igniteteams/src/screens/Players/index.tsx
+++ Ignite/react-native/modulos/igniteteams/src/screens/Players/index.tsx
@@ -1,6 +1,8 @@
 import { Header } from "@components/Header";
-import { Container } from "./styles";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Container, Form } from "./styles";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 
 export function Players() {
   return (
@@ -10,6 +12,13 @@ export function Players() {
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
+      <Form>
+        <Input
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+        <ButtonIcon icon="add" />
+      </Form>
     </Container>
   );
 }
