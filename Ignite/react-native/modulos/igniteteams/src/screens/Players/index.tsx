1c75f7dcead5506e036166bb0570ecdab5428c49
Josan Johnata <josan.pontes@meliuz.com.br>
2023-04-01 16:31:26 -0300

    feat: Add PlayerCard Components

--- Ignite/react-native/modulos/igniteteams/src/screens/Players/index.tsx
+++ Ignite/react-native/modulos/igniteteams/src/screens/Players/index.tsx
@@ -6,6 +6,9 @@ import { ButtonIcon } from "@components/ButtonIcon";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
@@ -45,6 +48,31 @@ export function Players() {
           {players.length}
         </NumbersOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard
+            name={item}
+            onRemove={() => {}}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message='Não há pessoas nesse time.'
+          />
+        )}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          !players.length && { flex: 1 }
+        ]}
+      />
+      <Button
+        title="Remover Turma"
+        type="SECONDARY"
+      />
     </Container>
   );
 }
