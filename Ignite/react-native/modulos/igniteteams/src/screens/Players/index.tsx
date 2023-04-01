1bccd5dfedcb99a6ce8cef0c16be6d27189f2fc0
Josan Johnata <josan.pontes@meliuz.com.br>
2023-04-01 12:57:10 -0300

    feat: Add Players Components

--- Ignite/react-native/modulos/igniteteams/src/screens/Players/index.tsx
+++ Ignite/react-native/modulos/igniteteams/src/screens/Players/index.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/Header";
 import { ButtonIcon } from "@components/ButtonIcon";
-import { Container, Form } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
 
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
+
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState([])
+
   return (
     <Container>
       <Header showBackButton />
@@ -20,7 +27,24 @@ export function Players() {
         />
         <ButtonIcon icon="add" />
       </Form>
-      <Filter title="Time A" />
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item })=> (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumbersOfPlayers>
+          {players.length}
+        </NumbersOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
