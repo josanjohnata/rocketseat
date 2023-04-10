0af6b416bf5f710f33024f302cb797366db3522c
Josan Johnata <josan.pontes@meliuz.com.br>
2023-04-10 20:36:42 -0300

    feat: Add routes

---
+++ Ignite/react-native/modulos/igniteteams/src/routes/app.routes.tsx
@@ -0,0 +1,26 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Groups } from '@screens/Groups';
+import { Players } from '@screens/Players';
+import { NewGroup } from '@screens/NewGroup';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+  return(
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen
+        name='groups'
+        component={Groups}
+      />
+      <Screen
+        name='players'
+        component={Players}
+      />
+      <Screen
+        name='new-group'
+        component={NewGroup}
+      />
+    </Navigator>
+  );
+}
