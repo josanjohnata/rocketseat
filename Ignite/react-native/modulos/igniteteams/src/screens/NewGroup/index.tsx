d1e7dffeb82c491831ca1cee196015ba56d2cfdd
Josan Johnata <josan.pontes@meliuz.com.br>
2023-04-15 16:53:06 -0300

    feat: Add go back function

--- Ignite/react-native/modulos/igniteteams/src/screens/NewGroup/index.tsx
+++ Ignite/react-native/modulos/igniteteams/src/screens/NewGroup/index.tsx
@@ -1,10 +1,19 @@
-import { Header } from "@components/Header";
+import { useNavigation } from "@react-navigation/native";
+
 import { Container, Content, Icon } from "./styles";
+
+import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate('players', { group: 'Rocket' });
+  };
+
   return (
     <Container>
       <Header showBackButton />
@@ -22,6 +31,7 @@ export function NewGroup() {
         <Button
           title="Criar"
           style={{ marginTop: 25 }}
+          onPress={handleNew}
         />
       </Content>
     </Container>
