5a076889425cf5c71ba828df18e7a73770f825e1
Josan Johnata <josan.pontes@meliuz.com.br>
2023-04-21 12:40:36 -0300

    feat: Adiciona render de NewGroups

--- Ignite/react-native/modulos/igniteteams/src/screens/Groups/index.tsx
+++ Ignite/react-native/modulos/igniteteams/src/screens/Groups/index.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+
+import { groupsGetAll } from '@storage/group/groupsGetAll';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -18,6 +20,20 @@ export function Groups() {
   function handleNewGroup() {
     navigation.navigate('new');
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll();
+      setGroups(data);
+
+    } catch (error) {
+      console.log(error);
+    }    
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchGroups();
+  }, [groups]));
 
   return (
     <Container>
